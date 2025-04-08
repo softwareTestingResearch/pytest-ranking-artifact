ec1f521e6ffcb00623d2143af8f0ffcf5d0f08c8
softwareTestingResearch <testingresearch4all@gmail.com>
2025-04-07 21:01:04 -0400

    ci files with uv

--- rerun_test_build_scripts/main.py
+++ rerun_test_build_scripts/main.py
@@ -4,7 +4,7 @@ import sys
 
 script_dir = os.path.dirname(__file__)
 parent_dir = os.path.join(script_dir, "..", "")
-local_dir = os.path.join(script_dir, "..", "rerun_test_build_scripts")
+local_dir = os.path.join(script_dir, "..", "sync_prs")
 sys.path.append(parent_dir)
 sys.path.append(local_dir)
 
@@ -14,11 +14,19 @@ def run_projects(command):
     project_infos = json.load(open("project_meta.json", "r"))
     for project in project_infos:
         os.system(f"python3 rerun_global_runs.py {project} {command}")
-        input(f"[global-run] Finish {project}, press enter to continue:")
+        # input(f"[global-run] Finish {project}, press enter to continue:")
+        pass
+
+def run_projects_random(command):
+    project_infos = json.load(open("project_meta.json", "r"))
+    for project in project_infos:
+        os.system(f"python3 rerun_random.py {project} {command}")
+        # input(f"[global-run] Finish {project}, press enter to continue:")
         pass
 
 if __name__ == "__main__":
-    run_projects(command="setup")
-    # run_projects(command="rerun")
-    # run_projects(command="download")
+    run_projects(command="setup,rerun")
+    run_projects(command="download")
+    run_projects_random(command="setup,rerun")
+    run_projects_random(command="download")
     pass
